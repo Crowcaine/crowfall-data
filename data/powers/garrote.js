d2d473c878615fa0c8dabf60a30eacbf9c29193a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-07 17:24:02 -0500

    feature: assassin powers c1-3 talents

---
+++ data/powers/garrote.js
@@ -0,0 +1,12 @@
+export default {
+  castTime: 'instant',
+  cooldown: 26,
+  cost: 1,
+  costType: 'Pips',
+  description:
+    'gashes your enemy for 46 damage, scaling with the amount of pips spent, and apply a Bleed. This attack also applies Poison to the target when used against an exposed target.\\nBleeding causes 41 Bleed damage every two seconds for 16 seconds.  Poison caues 54 Poison damage every second for 10 seconds.',
+  id: 'garrote',
+  name: 'Garrote',
+  range: 5,
+  target: 'reticle'
+};
---
+++ data/powers/kidney-shot.js
@@ -0,0 +1,12 @@
+export default {
+  castTime: 'instant',
+  cooldown: 22,
+  cost: 1,
+  costType: 'Pips',
+  description:
+    'attacks an enemy with a crippling blow to the vitals causing 148 Weapon Damage and stunning for a duration scaling by Pips spent.\\n\\n1 Pip: 3.25 seconds \\n2 Pips: 4 seconds \\n3 Pips: 4.75 seconds \\n4 Pips: 5.5 seconds \\n5 Pips: 6.25 seconds',
+  id: 'kidney-shot',
+  name: 'Kidney Shot',
+  range: 5,
+  target: 'reticle'
+};
