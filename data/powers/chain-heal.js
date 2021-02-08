d7e0b53f105924ebcb6347dceb6f4ce18c26c350
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 18:08:21 -0500

    feature: add friar powers

---
+++ data/powers/chain-heal.js
@@ -0,0 +1,14 @@
+export default {
+  castTime: 'instant',
+  cooldown: 9,
+  cost: 132,
+  costType: 'resource',
+  description:
+    'heals a targeted ally for 428 health and also healing a nearby ally.\nThe chain healing bonus jumps statistic adds additional targets by healing is reduced by 30% per target, up to a maximum of a 60% reduction.',
+  id: 'chain-heal',
+  name: 'Chain Heal',
+  radius: 10,
+  range: 35,
+  target: 'ray',
+  version: '6.400'
+};
---
+++ data/powers/fortuitous-blessings.js
@@ -0,0 +1,9 @@
+export default {
+  castTime: 'passive',
+  description:
+    "periodically grants the Seekers's Blessing for movement speed if you have not recently healer, Impoverished Bleesing for restoring your resource over time if you have healed and are low resource and Charitable Blessing for support power otherwise.",
+  id: 'fortuitous-blessings',
+  name: 'Fortuitous Blessings',
+  target: 'self',
+  version: '6.400'
+};
