f3c6d3db87d2d6093b74dac9a8d4f62d1e40ed58
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 17:24:36 -0500

    feature: add field surgeon powers

---
+++ data/powers/fly-you-fools.js
@@ -0,0 +1,11 @@
+export default {
+  castTime: 'passive',
+  description:
+    'grants a [stat:movement-speed:50] of 50% for 10 seconds, heals your group for 1125 health, and grants them a barrier when you die.',
+  duration: 10,
+  id: 'fly-you-fools',
+  name: 'Fly You Fools',
+  radius: 50,
+  target: 'self',
+  version: '6.400'
+};
