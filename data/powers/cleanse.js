e3501ef538af20ef8e145d3c5cf368b5472bd6c6
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 16:29:08 -0500

    feature: add naiad major powers

---
+++ data/powers/cleanse.js
@@ -0,0 +1,15 @@
+export default {
+  castTime: 'instant',
+  cooldown: 23,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'removes all bleeds, diseases, damnation, poisons and aura emitters from your group members and gives a short immunity to those removed. Increases your bleed resistances by 2000 for 16 seconds.',
+  duration: 16,
+  id: 'cleanse',
+  name: 'Cleanse',
+  maxTargets: 5,
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
---
+++ data/powers/fountain-of-life.js
@@ -0,0 +1,15 @@
+export default {
+  castTime: 'instant',
+  cooldown: 20,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'create a fountain of life at the targeted location which chain heals nearby allies for 171. This lasts for 15 seconds or 40 heals.',
+  duration: 15,
+  id: 'fountain-of-life',
+  name: 'Fountain of Life',
+  radius: 5,
+  range: 35,
+  target: 'area',
+  version: '6.400'
+};
