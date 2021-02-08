c3baa386829d18fe052cb920ffed7d8bede4894f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 18:17:52 -0500

    feature: add cloister powers

---
+++ data/powers/spite.js
@@ -0,0 +1,9 @@
+export default {
+  castTime: 'passive',
+  description:
+    'recovers 187 health every 2 seconds over 4 seconds each time your suffer a knockdown, stun, blind or supress.',
+  id: 'spite',
+  name: 'Spite',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/stats/benediction.js
@@ -0,0 +1,15 @@
+export default {
+  castTime: 'instant',
+  cooldown: 45,
+  cost: 72,
+  costType: 'resource',
+  description:
+    'blesses group members within 50m and you increasing [stat:resistance-organic:2500] by 2500 and [stat:cooldown-long:10] by 10% for 30 seconds.',
+  duration: 30,
+  id: 'benediction',
+  maxTargets: 5,
+  name: 'Benediction',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
