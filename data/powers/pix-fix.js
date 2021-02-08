6c043e34fccf1da992354e39b8cd2436575ef462
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 17:02:51 -0500

    feature: add pixie powers

---
+++ data/powers/pix-fix.js
@@ -0,0 +1,9 @@
+export default {
+  castTime: 'passive',
+  description:
+    'gains one additional target for all chain type healing abilities and removes the healing falloff per target.',
+  id: 'pix-fix',
+  name: 'Pix Fix',
+  target: 'self',
+  version: '6.400'
+};
