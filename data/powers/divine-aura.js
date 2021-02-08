40797084d89cc89a82d8ee9b002c17b51aaabe06
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 16:35:19 -0500

    feature: add transcendent warrior powers

---
+++ data/powers/divine-aura.js
@@ -0,0 +1,12 @@
+export default {
+  castTime: 'toggle',
+  cooldown: '1',
+  description:
+    'fortifies group members and you with otherworldly strength increasing their [stat:support-power:75] by 75 and [stat:personal-healing-modifier:10] by 10% while you maintain the effect.',
+  id: 'divine-aura',
+  name: 'Divine Aura',
+  maxTargets: 5,
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
---
+++ data/powers/guiding-light-aura.js
@@ -0,0 +1,12 @@
+export default {
+  castTime: 'toggle',
+  cooldown: '1',
+  description:
+    'foritifies group members and you with otherworldly strength increasing their maximum [stat:damage-fire-cap:10] and [stat:damage-holy-cap:10] by 10% while you maintain the effect.',
+  id: 'guiding-light-aura',
+  name: 'Guiding Light Aura',
+  maxTargets: 5,
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
