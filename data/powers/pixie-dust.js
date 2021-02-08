c1d8b84be7552e0d39a598911c45575d4322b989
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 16:23:09 -0500

    feature: add forest creature powers

---
+++ data/powers/pixie-dust.js
@@ -0,0 +1,15 @@
+export default {
+  castTime: 'instant',
+  cooldown: 45,
+  cost: 84,
+  costType: 'resource',
+  description:
+    'blankets the targetted area with Pixie Dust, increasing [stat:personal-healing-modifier:25%] by 25% for 30 seconds',
+  duration: 30,
+  id: 'pixie-dust',
+  name: 'Pixie Dust',
+  maxTargets: 5,
+  radius: 5,
+  target: 'area',
+  version: '6.400'
+};
---
+++ data/powers/slow-stake.js
@@ -0,0 +1,15 @@
+export default {
+  castTime: 'instant',
+  cooldown: 45,
+  cost: 192,
+  costType: 'resource',
+  description:
+    'creates a field, snaring any enemies in it every second for 15 seconds as long as they remain near the stake.',
+  duration: 15,
+  id: 'slow-stake',
+  name: 'Slow Stake',
+  maxTargets: 5,
+  radius: 10,
+  target: 'self',
+  version: '6.400'
+};
