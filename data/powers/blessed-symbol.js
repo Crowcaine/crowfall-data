f119061542037b7fd1c0ad00c20ef52bdcba01be
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 17:57:03 -0500

    feature: add exorcist powers

---
+++ data/powers/blessed-symbol.js
@@ -0,0 +1,9 @@
+export default {
+  castTime: 'passive',
+  description:
+    'inceases your [stat:critial-healing:15] by 15% and [stat:critical-healing-amount:15] by 15% for 20 seconds when you drop below 50% health. This cannot occur more than once every 30 seconds.',
+  id: 'blessed-symbol',
+  name: 'Blessed Symbol',
+  target: 'self',
+  version: '6.400'
+};
