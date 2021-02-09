973cc40709159994f297c358f8fa8ac2df8c57ee
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:35:51 -0500

    feature: add force mage powers

---
+++ data/powers/force-wave.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 24,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'pushes enemies away from you inflicting 306 damage and leaves their movement slowed.',
+  id: 'force-wave',
+  maxTargets: 5,
+  name: 'Force Wave',
+  radius: 5,
+  target: 'area',
+  version: '6.400'
+};
---
+++ data/powers/reactive-barrier.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'grants a chance for attacks to generate a force shield giving you a barrier and increaseing [stat:attack-power:100] by 100 and your [stat:support-power:200] by 200.\nThis can only occur once every 20 seconds.',
+  id: 'reactive-barrier',
+  name: 'Reactive Barrier',
+  target: 'self',
+  version: '6.400'
+};
