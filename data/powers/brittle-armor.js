d8a0af9a7948e96cd1d88db2c4a35c6f3b5a2672
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:08:25 -0500

    feature: add tundra mage powers

---
+++ data/powers/brittle-armor.js
@@ -0,0 +1,14 @@
+export default {
+  castTime: 'instant',
+  cooldown: 30,
+  cost: 84,
+  costType: 'resource',
+  description:
+    'causes 226 damage and debuffs their final physical mitigation by 20 for 30 seconds.',
+  duration: 30,
+  id: 'brittle-armor',
+  name: 'Brittle Armor',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
---
+++ data/powers/freezing-storm.js
@@ -0,0 +1,14 @@
+export default {
+  castTime: 'ground aimed',
+  cooldown: 15,
+  cost: 80,
+  costType: 'resource',
+  description:
+    'unleashes a freezing storm under the ground targeted area repeatedly inflicting 119 damage to enemies and slowing them. Applies a stack of frezzing every second, reaching 5 stacks of freezing will stun the target.',
+  id: 'freezing-storm',
+  maxTargets: 5,
+  name: 'Freezing Storm',
+  range: 30,
+  target: 'area',
+  version: '6.400'
+};
