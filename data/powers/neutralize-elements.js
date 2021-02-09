ce0955cbca5876840e7ed7b1e7035f1f6878e36a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:47:55 -0500

    feature: add elementalist powers

---
+++ data/powers/neutralize-elements.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'removes all burning, frostbite, electrocution, corruption and poison from your group members and gives a short immunity to those removed. Increases your [stat:resistance-elemental:1500] by 1550 for 16 seconds.',
+  duration: 16,
+  id: 'neutralize-elements',
+  maxTargets: 5,
+  name: 'Neutralize Elements',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
