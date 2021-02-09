0247de4bc4971989b646e99db9cfb6467d8aa012
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:27:46 -0500

    feature: add master of focus powers

---
+++ data/powers/chaos-orb.js
@@ -0,0 +1,12 @@
+export default {
+  castTime: 'charged',
+  cooldown: 9,
+  cost: 144,
+  costType: 'resource',
+  description:
+    'conjures a slow moving doom-filled projectile that passes through enemies to damage them repeatedly for 460 damage.',
+  id: 'chaos-orb',
+  name: 'Chaos Orb',
+  target: 'reticle',
+  version: '6.400'
+};
---
+++ data/powers/mana-shield.js
@@ -0,0 +1,13 @@
+export default {
+  castTime: 'instant',
+  cooldown: 23,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'surrounds you with a protective barrier draining your resource as it prevents damage for 12 seconds.',
+  duration: 12,
+  id: 'mana-shield',
+  name: 'Mana Shield',
+  target: 'self',
+  version: '6.400'
+};
