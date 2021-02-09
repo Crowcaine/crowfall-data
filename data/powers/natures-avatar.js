22cdfd76588602e0c290239f910da3cc56287123
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 20:29:10 -0500

    bug: first naturues avatar name

--- data/powers/natures-avatar.js
+++ data/powers/natures-avatar.js
@@ -6,7 +6,7 @@ export default {
   description:
     'empowers a targeted ally increasing [stat:damage:20] by 20%, [stat:critical-strike:20] by 20%, [stat:critical-damage:20] by 20%, [stat:personal-damage-modifier:20] by 20% and [stat:power-damage-melee:20] by 20% while you channel.',
   id: 'natures-avatar',
-  name: 'Naturess Avatar',
+  name: "Nature's Avatar",
   range: 50,
   target: 'reticle',
   version: '6.400'
