8e52dfe970025a54d6ebff4f272b7aa04dc72391
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:00:43 -0500

    bug: move benediction and in-the-zone from stats

--- data/powers/in-the-zone.js
+++ data/powers/in-the-zone.js
@@ -3,8 +3,9 @@ export default {
   cost: 1,
   costType: 'pips',
   description:
-    'increases your [stat:power-efficiency:20] by 20%, and increases your [stat:power-damage-behind:10] by 10% for 5 per pip spent.',
+    'increase your [stat:power-efficiency:20] by 20% and increases your [stat:power-damage-behind:10] by 10% for 5 per pip spent.',
   id: 'in-the-zone',
   name: 'In The Zone',
-  target: 'self'
+  target: 'self',
+  version: '6.400'
 };
--- data/stats/in-the-zone.js
+++
@@ -1,9 +0,0 @@
-export default {
-  castTime: 'instant',
-  description:
-    'increase your [stat:power-efficiency:20] by 20% and increases your [stat:power-damage-behind:10] by 10% for 5 per pip spent.',
-  id: 'in-the-zone',
-  name: 'In The Zone',
-  target: 'self',
-  version: '6.400'
-};
