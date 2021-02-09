7a1bc66adfb7c482e02df052818088c537f05df3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:15:25 -0500

    feature: add reality warper powers

---
+++ data/powers/shrivel.js
@@ -0,0 +1,13 @@
+export default {
+  castTime: 'instant',
+  cooldown: 45,
+  cost: 72,
+  costType: 'resource',
+  description:
+    'curses an enemy with disease and blackmantle, absorbing up to 500 points of healing.\nIf Shrivel hits an Exposed enemy while they attack or use an ability, they are knocked down and take additional damage.\nDisease caues 230 disease damage every two seconds for 10 seconds.',
+  id: 'shrivel',
+  name: 'Shrivel',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
---
+++ data/powers/spiritual-decay.js
@@ -0,0 +1,15 @@
+export default {
+  castTime: 'instant',
+  cooldown: 45,
+  cost: 78,
+  costType: 'resource',
+  description:
+    'decays the area within 8 meters around you every 3 seconds for 12 seconds, all targets hit have their healing bonus decreased by 25%.',
+  duration: 12,
+  id: 'spiritual-decay',
+  maxTargets: 5,
+  name: 'Spiritual Decay',
+  radius: 8,
+  target: 'self',
+  version: '6.400'
+};
