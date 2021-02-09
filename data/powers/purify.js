0259018959df22ba279261e3f3a7442108b1f633
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 20:08:27 -0500

    feature: add bringer of life powers

---
+++ data/powers/purify.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  costType: 'resource',
+  description:
+    'removes all blackmantle, frostbite, pulsing damage aura, damnation and disease from your group members and gives you a short immunity to those removed. Increases your [stat:resistance-physical:1000] by 1000 for 16 seconds.',
+  duration: 16,
+  id: 'purify',
+  maxTargets: 5,
+  name: 'Purify',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
