5f87329f80ef8c8d82fc8a87d769bb839c13744c
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 20:25:56 -0500

    feature: add hydrologist powers

---
+++ data/powers/cauterize.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'removes all bleeds, electrocution, burning, corruption and blackmantle from your group members and gives a short immunity to those removed. Increases your [stat:resistances-organic:1500] for 16 seconds.',
+  duration: 16,
+  id: 'cauterize',
+  maxTargets: 5,
+  name: 'Cauterize',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
---
+++ data/powers/water-spirits.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'charged',
+  cooldown: 16,
+  cost: 174,
+  costType: 'resource',
+  description:
+    'summons and launch a water spirit forward which heals allies within range of the water spirit.\nWater spirits last 20 seconds or 30 heals.\nWater spirits only heals each player a maximum of 5 times.\n Enemies hit by the water spirit suffer a 30% to [stat:damage-fire:30%] for 20 seconds.',
+  duration: 20,
+  id: 'water-spirits',
+  maxTargets: 5,
+  movement: 4,
+  name: 'Water Spirits',
+  target: 'area',
+  version: '6.400'
+};
