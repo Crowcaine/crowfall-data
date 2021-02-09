53a6e53c0a0b9a0887d3dd8d3a1ce455a6f7849a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:41:49 -0500

    feature: add frostbiter powers

---
+++ data/powers/decay.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 72,
+  costType: 'resource',
+  description:
+    'inflicts an enemy with frostbite. Frostbite causes 163 ice damage seven times over 30 seconds. If decay hits and exposed enemy while they attack or use an ability, they are knocked down.',
+  id: 'decay',
+  name: 'Decay',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
---
+++ data/powers/rotting-frost.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description:
+    'applies a blackmantle debuff any time you apply a frostbite damage over time.',
+  id: 'rotting-frost',
+  name: 'Rotting Frost',
+  target: 'self'
+};
