c08cab9caa4132809def9e3b28e13f557fbfc0e4
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 20:40:48 -0500

    feature: add faerie trapper powers

---
+++ data/powers/concussive-trap.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 32,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'places a concussive trap at your feet which explodes when an enemy approaches it dealing 99 damage.\n All targets hit are stunned.\nTrap will exist for 60 seconds.',
+  duration: 30,
+  id: 'concussive-trap',
+  name: 'Concussive Trap',
+  range: 20,
+  target: 'area',
+  version: '6.400'
+};
---
+++ data/powers/faerie-fire.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  cooldown: 47,
+  description:
+    'lines an enemy in faerie fire decreasing their [stat:anti-critical-strike:8] by 8% for 30 seconds. The target cannot stealth while affected.',
+  duration: 30,
+  id: 'faerie-fire',
+  name: 'Faerie Fire',
+  range: 20,
+  target: 'ray',
+  version: '6.400'
+};
