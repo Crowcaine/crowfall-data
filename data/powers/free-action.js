8537dade512543992d90fe041276352e8b7aa54d
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 20:16:40 -0500

    feature: add freedom fighter powers

---
+++ data/powers/free-action.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 72,
+  costType: 'resource',
+  description:
+    'liberates group members and yourself gaining immunity to crowd control effects for 9 seconds.\nThis attack can be instantly flash-cast.',
+  duration: 9,
+  id: 'free-action',
+  name: 'Free Action',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
