2f766ab51d02a8ba2e238304409d83d495ae7e2a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-08 19:23:14 -0500

    feature: add petrifier powers

---
+++ data/powers/nothing-to-lose.js
@@ -0,0 +1,13 @@
+export default {
+  castTime: 'instant',
+  cooldown: 23,
+  cost: 500,
+  costType: 'health',
+  description:
+    'sacrifices 500 health to increase [stat:critical-strike:10] by 10%, [stat:critical-damage:10] by 10% and [stat:power-damage-melee:5] by 5% for 15 seconds.',
+  duration: 15,
+  id: 'nothing-to-lose',
+  name: 'Nothing To Lose',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/petrification-illusion.js
@@ -0,0 +1,13 @@
+export default {
+  castTime: 'instant',
+  cooldown: 23,
+  cost: 96,
+  costType: 'resource',
+  description:
+    'increases [stat:basic-attack:10] by 10% for 15 seconds and modifies your their basic attack to slow enemies.',
+  duration: 15,
+  id: 'petrification-illusion',
+  name: 'Petrification Illusion',
+  target: 'self',
+  version: '6.400'
+};
