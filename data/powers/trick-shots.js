050ae4df5197814754fdee4cf312d5bcd39116ea
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-10 10:52:54 -0500

    Update data/powers/trick-shots.js

--- data/powers/trick-shots.js
+++ data/powers/trick-shots.js
@@ -1,8 +1,6 @@
 export default {
   castType: 'passive',
-  description: 'demonstrates your prowess with a bow, allowing full movement speed while charging Bow Basic attacks.
-
-Trick Shots also changes your Double Shots passive into Triple Shots which causes aditional 25% damage.',
+  description: 'demonstrates your prowess with a bow, allowing full movement speed while charging Bow Basic attacks.\nTrick Shots also changes your Double Shots passive into Triple Shots which causes aditional 25% damage.',
   id: 'trick-shots',
   name: 'Trick Shots',
   target: 'self',
