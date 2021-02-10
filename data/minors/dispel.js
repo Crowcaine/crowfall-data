16f84647d6d1f454c80fe616ea4f5ab66f1361c7
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-09 22:05:59 -0500

    bug: dispel had incorrect major name

--- data/minors/dispel.js
+++ data/minors/dispel.js
@@ -2,7 +2,7 @@ export default {
   description: '',
   grants: ['stat:attack-power:50', 'power:dispel'],
   id: 'dispel',
-  name: 'Dirge of Dissonance',
+  name: 'Dispel',
   requirements: ['domain:protection'],
   type: 'minor',
   version: '6.400'
