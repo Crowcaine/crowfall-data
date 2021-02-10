fd8fecb54d76f6c967e23c58d8ef93d519c79b80
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-10 10:49:29 -0500

    Update data/powers/cross-slash-ii.js

--- data/powers/cross-slash-ii.js
+++ data/powers/cross-slash-ii.js
@@ -3,9 +3,7 @@ export default {
   cooldown: 1,
   cost: 157,
   costType: 'energy',
-  description: 'slashes in front of you for 226 damage. Applies a Bleed effect.
-
-Bleeding causes 160 damage every two seconds for 16 seconds.',
+  description: 'slashes in front of you for 226 damage. Applies a Bleed effect.\nBleeding causes 160 damage every two seconds for 16 seconds.',
   id: 'cross-slash-ii',
   name: 'Cross Slash',
   range: 5,
