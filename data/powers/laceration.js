bcc22c1d05127500b7e88028add44498e4e8b3e2
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-10 10:58:21 -0500

    Update laceration.js

--- data/powers/laceration.js
+++ data/powers/laceration.js
@@ -3,7 +3,7 @@ export default {
   cooldown: 23,
   cost: 157,
   costType: 'energy',
-  description: 'slashes in front of you twice for 113 damage. Applies a Weapon Break which reduces that target's damage by 20% for 15 seconds.',
+  description: "slashes in front of you twice for 113 damage. Applies a Weapon Break which reduces that target's damage by 20% for 15 seconds.",
   id: 'laceration',
   name: 'Laceration',
   range: 5,
