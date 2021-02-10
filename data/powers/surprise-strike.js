03755b3584423b9dad93885b9a070289104a4b24
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-10 10:52:33 -0500

    Update data/powers/surprise-strike.js

--- data/powers/surprise-strike.js
+++ data/powers/surprise-strike.js
@@ -1,9 +1,7 @@
 export default {
   castType: 'instant',
   cooldown: 9,
-  description: 'delivers a lethal surprise attack from the shadows causing 437 damage and applies a Bleed.
-
-Bleeding causes 295 Bleed damage every two seconds for 16 seconds.',
+  description: 'delivers a lethal surprise attack from the shadows causing 437 damage and applies a Bleed.\nBleeding causes 295 Bleed damage every two seconds for 16 seconds.',
   id: 'surprise-strike',
   name: 'Surprise Strike',
   range: 5,
