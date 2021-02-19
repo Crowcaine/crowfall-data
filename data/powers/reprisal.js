6f6dddb6384b845ccd05d0df049bf415e550e963
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-19 00:59:19 -0500

    feature: add adjudicator powers

---
+++ data/powers/reprisal.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 30,
+  cost: 12,
+  costType: 'resource',
+  description:
+    'absorbs 10% of damage taken for 15 seconds and then explodes to inflicting 35% of total damage taken (max 1050) to nearby enemies as fire damage.',
+  duration: 15,
+  id: 'reprisal',
+  maxTargets: 5,
+  name: 'Reprisal',
+  radius: 5,
+  target: 'self',
+  version: '6.400'
+};
