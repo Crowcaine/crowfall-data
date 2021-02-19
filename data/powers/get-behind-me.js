107e19966933e1e13ec1cf2417a9c53029949889
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-19 00:36:46 -0500

    feature: add stalwart powers

---
+++ data/powers/get-behind-me.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  costType: 'resource',
+  description:
+    'grants your group members a [stat:personal-damage-modifier:0] of 0% while you are blocking.',
+  id: 'get-behind-me',
+  maxTargets: 5,
+  name: 'Get Behind Me',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
---
+++ data/powers/molon-labe.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'returns fire, ice or electric damage back upon an attacker. Knights also gain a 5% damage bonus after blocking big hits.',
+  id: 'molon-labe',
+  name: 'Molon Labe',
+  target: 'self',
+  version: '6.400'
+};
