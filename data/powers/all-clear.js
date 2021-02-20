52342223a611899f321d4530997b197d7ddf49d3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-19 20:05:22 -0500

    feature: add sharpshooter powers

---
+++ data/powers/all-clear.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases [stat:damage:10] by 10% when there are no enemies within 30m.',
+  id: 'all-clear',
+  name: 'All Clear',
+  target: 'self',
+  version: '6.400'
+};
