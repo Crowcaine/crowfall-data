bc76b06566e0c4906cf0e5ee8270399a2299c7fe
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-19 20:10:17 -0500

    feature: add reaver disciple powers

---
+++ data/powers/gash.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 24,
+  cost: 24,
+  costType: 'rage',
+  description:
+    'deals 312 damage and applies a bleed.\nBleeding causes 98 bleed damage every two seconds for 16 seconds.',
+  id: 'gash',
+  name: 'Gash',
+  range: 5,
+  target: 'cone',
+  version: '6.400'
+};
---
+++ data/powers/invincible-warrior.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  cooldown: 2,
+  cost: 500,
+  costType: 'soul power',
+  description:
+    'makes you nigh invincible with damage prevention and healing over time.\nBriefly reduces damage taken, removes expose statues and damage over time effects. This power can be used while suggering from control effects to escape and can be instantly flash-cast.',
+  id: 'invincible-warrior',
+  name: 'Invicible Warrior',
+  target: 'self',
+  version: '6.400'
+};
