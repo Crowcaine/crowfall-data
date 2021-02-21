338ab081a049c0a934bf8f237df3a94f1fc3d38b
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:00:07 -0500

    feature: add banshee discipline powers

---
+++ data/powers/haunt.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'haunt 5 enemies, reflecting 35% of their damage and healing you for 35% of damage taken, The Haunt debuff lasts 30 seconds or 5 hits and applies a curse to the enemy. The curse reduces their [stat:damage:10] by 10% and [stat:attack-power:75] by 75 for 30 seconds.',
+  duration: 30,
+  id: 'haunt',
+  maxTargets: 5,
+  name: 'Haunt',
+  radius: 5,
+  target: 'cone',
+  version: '6.400'
+};
