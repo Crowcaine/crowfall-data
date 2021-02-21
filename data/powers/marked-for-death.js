4d53ead57910688e688401659c5a211cbddcc8ca
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 17:26:11 -0500

    feature: add speed demon discipline powers

---
+++ data/powers/marked-for-death.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'places a mark on your target, you and your group members deal additional weapon damage to the target when you hit it for 20 seconds.',
+  duration: 20,
+  id: 'marked-for-death',
+  name: 'Marked for Death',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
