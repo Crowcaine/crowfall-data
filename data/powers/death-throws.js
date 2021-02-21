8984dc3fc8f239f577b7f76adb9c1c713af51262
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:50:21 -0500

    feature: add scrapper discipline power

---
+++ data/powers/death-throws.js
@@ -0,0 +1,11 @@
+export default {
+  castType: 'passive',
+  description:
+    'knocks down nearby enemies when you use a Retaliate ability',
+  id: 'death-throws',
+  maxTargets: 3,
+  name: 'Death Throws',
+  radius: 5,
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/in-the-fray.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 35,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'buffs your [stat:attack-power:15] and [stat:lifesteal:2] by 2 for each enemy within 20m, max of 15 seconds',
+  duration: 15,
+  id: 'in-the-fray',
+  name: 'In the Fray',
+  target: 'self',
+  version: '6.400'
+};
