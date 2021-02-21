107c450b639d20bbdf73de429b957aed53dea82c
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 17:58:25 -0500

    feature: add mass dispel power

---
+++ data/powers/mass-dispel.js
@@ -0,0 +1,16 @@
+export default {
+  castType: 'instant',
+  cooldown: 24,
+  cost: 57,
+  costType: 'resource',
+  description:
+    'removes all barrier from enemies in the targeted area and deal 125 damage. Enemies that have their barriers remove this way are exposed for 30 seconds.',
+  duration: 30,
+  id: 'mass-dispel',
+  maxTargets: 8,
+  name: 'Mass Dispel',
+  radius: 5,
+  range: 20,
+  target: 'area',
+  version: '6.400'
+};
