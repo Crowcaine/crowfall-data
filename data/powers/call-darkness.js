db6abb83691dee0d49fd065b153c626a0fe1e788
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:24:56 -0500

    feature: add call-darkness power

---
+++ data/powers/call-darkness.js
@@ -0,0 +1,11 @@
+export default {
+  castType: 'instant',
+  cost: 1,
+  costType: 'pips',
+  description:
+    'causes all attacks to count as if performed from behind the target and increases your [stat:power-damage-behind:15] by 15% with the duration of the power scaling with amount of pips spent.\n1 Pip: 4 seconds\n2 Pips: 8 seconds\n3 Pips: 12 seconds\n4 Pips: 16 seconds\n5 Pips: 20 seconds',
+  id: 'call-darkness',
+  name: 'Call Darkness',
+  target: 'self',
+  version: '6.400'
+};
