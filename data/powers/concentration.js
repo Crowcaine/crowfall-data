a48417ecc0316f06341e2961241bf4469cf0ae1f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 18:09:55 -0500

    feature: add redeemer discipline powers

---
+++ data/powers/concentration.js
@@ -0,0 +1,10 @@
+export default {
+  castType: 'passive',
+  description:
+    'restores 80 mana every 2 seconds until you move after standing still 2 seconds.',
+  duration: 2,
+  id: 'concentration',
+  name: 'Concentration',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/redemption.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'hold channeled',
+  cooldown: 9,
+  cost: 95,
+  costType: 'mana',
+  description:
+    "throws up to six fireballs in a row for 200 fire damage scaling up to 263 damage base on the enemy's sin.",
+  duration: 15,
+  id: 'redemption',
+  name: 'Redemption',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
