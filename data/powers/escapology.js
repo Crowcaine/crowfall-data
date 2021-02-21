4881905512fc45b0053ddcd62fa0c85eeecad25a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 14:52:48 -0500

    feature: add escapology power

---
+++ data/powers/escapology.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'adds 50 resolve when you are effected by knockdown, stun, blind, suppress, cooldown reduction or push effects.',
+  id: 'escapology',
+  name: 'Escapology',
+  target: 'self',
+  version: '6.400'
+};
