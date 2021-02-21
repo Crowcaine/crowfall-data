9997a84b1079e810348bd439c170fa118131cbeb
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 15:55:51 -0500

    feature: add dealy infector discipline powers

---
+++ data/powers/diffusion.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 6,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'deals 105 damage to you rtarget. Damage scale up to 1183 damage with more than 2 poison active.',
+  id: 'diffusion',
+  name: 'Diffusion',
+  range: 5,
+  target: 'reticle',
+  version: '6.400'
+};
