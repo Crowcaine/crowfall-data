d30ab0b890178fc9dfaf112f567ff7d576266f3f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 18:02:03 -0500

    feature: add militant mage discipline powers

---
+++ data/powers/sword-ward.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'enchants your armor with a ward against slashing attacks increasing your [stat:resistance-slashing:500] by 500 each time you are hit. Stacks up to 4 times.',
+  id: 'sword-ward',
+  name: 'Sword Ward',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/warp-weapons.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 30,
+  cost: 76,
+  costType: 'resource',
+  description:
+    'curses an enemy with a destructive aura decreasing their [stat:attack-power:1] by 1% for 15 seconds for each attack they commit against you while active.',
+  duration: 15,
+  id: 'warp-weapons',
+  name: 'Warp Weapons',
+  range: 15,
+  target: 'ray',
+  version: '6.400'
+};
