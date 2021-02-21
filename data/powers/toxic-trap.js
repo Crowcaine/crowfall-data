e630fd9f3d83da4fb9d2c1953827bcfada62dae4
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:05:58 -0500

    feature: add toxic trap power

---
+++ data/powers/toxic-trap.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description: 'adds a  Blackmantle debuff to all of your traps.',
+  id: 'toxic-trap',
+  name: 'Toxic Trap',
+  target: 'self',
+  version: '6.400'
+};
