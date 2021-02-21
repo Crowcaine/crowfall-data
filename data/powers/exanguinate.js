e7d28e091686bb0349fa1ac567c4fad5ac4a02c3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 14:38:38 -0500

    feature: add bloodletter discipline powers

---
+++ data/powers/exanguinate.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases the duration and damage of bleeds you apply.',
+  id: 'exanguinate',
+  name: 'Exanguinate',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/purgative.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  cooldown: 28,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'cleanses yourself of damage over time effects healing for 300 damage for each effect type and gives a short immunity to those removed.',
+  id: 'purgative',
+  name: 'Purgative',
+  target: 'self',
+  version: '6.400'
+};
