de4ce4db970a88bc1c18931ccf1af35f5f5c4129
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 14:21:52 -0500

    feature: add dirty fighter discipline powers

---
+++ data/powers/festering-wounds.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    "plagues an enemy and causes them to bleed and suffer from disease. For 30 seconds, the plagues enemy's healing modifier is reduce by 25, incoming healing is reduced by 10% and any healing ability leaves them in the exposed state.\nBleeding causes 250 bleed damage every two seconds for 16 seconds.\nDisease causes 399 disease damage every two seconds for 10 seconds.",
+  id: 'festering-wounds',
+  name: 'Festering Wounds',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
---
+++ data/powers/subvert-expectations.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'applies attack power with the buff amount increasing as your resource pool decreases.',
+  id: 'subvert-expectations',
+  name: 'Subvert Expectations',
+  target: 'self',
+  version: '6.400'
+};
