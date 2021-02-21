5582b78b2128ed074de9e79cff94a94e68eb3a91
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 15:50:22 -0500

    feature: add poison sapper discipline powers

---
+++ data/powers/poison-paradise.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases your lifesteal by 8% whenever you have a poison toxin equipped.',
+  id: 'poison-paradise',
+  name: 'Poison Paradise',
+  target: 'self',
+  version: '6.400'
+};
