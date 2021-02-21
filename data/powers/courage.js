a124467ce41e836bacbbbd6b48fcd18e48861da4
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 18:13:39 -0500

    feature: add commander discipline powers

---
+++ data/powers/courage.js
@@ -0,0 +1,10 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases your [stat:lifesteal:10] by 10 for 10 seconds when a barrier is placed on you.',
+  duration: 10,
+  id: 'courage',
+  name: 'Courage',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/hunker-down.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 57,
+  costType: 'resource',
+  description:
+    'deploys a field increasing your [stat:resistance-all:1000] by 1000 and restoring 5 stamina every second.',
+  duration: 15,
+  id: 'hunker-down',
+  name: 'Hunker Down',
+  radius: 4,
+  target: 'self',
+  version: '6.400'
+};
