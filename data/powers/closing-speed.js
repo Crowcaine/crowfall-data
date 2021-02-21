8d12136c9e3e0e816f7f892e0b77d3a4a2220fcb
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 17:28:48 -0500

    feature: add speed demon discipline powers

---
+++ data/powers/closing-speed.js
@@ -0,0 +1,10 @@
+export default {
+  castType: 'passive',
+  description:
+    'grants a [stat:movement-speed-modifier:2] increase by 2% for duration each time you are hit with a ranged attack as often as twice per second.',
+  duration: 5,
+  id: 'closing-speed',
+  name: 'Closing Speed',
+  target: 'self',
+  version: '6.400'
+};
