723fc5b3d802858dc3b9bcf2b7cb6bdafaad050a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 14:13:58 -0500

    feature: add hellraiser discipline powers

---
+++ data/powers/needlin-haystack.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 35,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'covers your body with sharp needle-like straws increasing your [stat:resistance-piercing:2500] for 12 seconds.\nEnemies around you take 15% increase damage.',
+  duration: 12,
+  id: 'needlin-haystack',
+  maxTargets: 5,
+  name: "Needlin' Haystack",
+  range: 5,
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/pin-cushion.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'causes you to gain a piercing damage immune barrier for 15 seconds.',
+  duration: 15,
+  id: 'pin-cushion',
+  name: 'Pin Cushion',
+  target: 'self',
+  version: '6.400'
+};
