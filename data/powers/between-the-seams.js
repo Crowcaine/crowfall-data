332e18d9e0c35b9fe94e8c1dd2d10c1575af3d18
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 17:06:37 -0500

    feature: add between the seams power

---
+++ data/powers/between-the-seams.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'enhances group members and you increasing [stat:armor-penetration-piercing:15] by 15% for 30 seconds.',
+  duration: 30,
+  id: 'between-the-seams',
+  maxTargets: 5,
+  name: 'Between the Seams',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
