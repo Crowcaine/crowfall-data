d06ca1a0d5808fe1da117a3e92c1ea66600d3e04
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:18:05 -0500

    feature: add unholy beast discipline power

---
+++ data/powers/everlooming.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description: 'grants immunity to slow effects.',
+  id: 'everlooming',
+  name: 'Everlooming',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/undead-fortitude.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description: 'grants immunity to Blankmantle effects.',
+  id: 'undead-fortitude',
+  name: 'Undead Fortitude',
+  target: 'self',
+  version: '6.400'
+};
