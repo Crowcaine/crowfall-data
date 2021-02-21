903f7354e62178ccfa80340a0aedd14cdf0bd44b
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 17:03:38 -0500

    feature: add master of darkness discipline powers

---
+++ data/powers/preparation.js
@@ -0,0 +1,10 @@
+export default {
+  castType: 'passive',
+  description:
+    "allows you to plan your next attack when entering stealth, immediately recovering a pip if you don't have one, and periodically gaining additional pips every 30 seconds as you maintain stealth.",
+  duration: 30,
+  id: 'preparation',
+  name: 'Preparation',
+  target: 'self',
+  version: '6.400'
+};
