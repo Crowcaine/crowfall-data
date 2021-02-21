8028829aaab1a59da12c04ef64a6a8b063730bc3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 15:11:35 -0500

    feature: add hamstring power

---
+++ data/powers/hamstring.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'applies a slow when attacking an enemy that is currently bleeding.',
+  id: 'hamstring',
+  name: 'Hamstring',
+  target: 'self',
+  version: '6.400'
+};
