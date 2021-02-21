b7235cb9be1fd0ef1cdbf683846844c721ed0781
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 14:59:14 -0500

    feature: add arcane-archer discipline powers

---
+++ data/powers/arcane-arrow.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  cooldown: 29,
+  cost: 2,
+  costType: 'resource',
+  description:
+    "launches an arcane arrow where you aim, dealing 137 damage to up to 5 enemies in it's path and applies an elemental break to enemies it hits.",
+  id: 'arcane-arrow',
+  name: 'Arcane Arror',
+  target: 'ray',
+  version: '6.400'
+};
---
+++ data/powers/arcane-barrier.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'protects you with a barrier preventing up to 1116 damage. Reduces incoming elemental damage by 80% while the barrier is active. The barrier lasts 15 seconds or until the barrier is depleted, whichever comes first.',
+  duration: 15,
+  id: 'arcane-barrier',
+  name: 'Arcane Barrier',
+  target: 'self',
+  version: '6.400'
+};
