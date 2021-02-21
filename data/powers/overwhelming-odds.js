2516cf67b62c080600d0b626c4304025165da65a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 18:17:14 -0500

    feature: add fortifier discipline powers

---
+++ data/powers/overwhelming-odds.js
@@ -0,0 +1,10 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases your [stat:resistance-all:10] by 10% for 15 seconds when three of more enemies are within 10m.\nThis does not increase bleed mitigation bonus.',
+  duration: 15,
+  id: 'overwhelming-odds',
+  name: 'Overwhelming Odds',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/sanctuary.js
@@ -0,0 +1,16 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 57,
+  costType: 'resource',
+  description:
+    'increases your [stat:lifesteal:10] by 10 for 10 seconds when a barrier is placed on you.',
+  duration: 4,
+  id: 'sanctuary',
+  maxTargets: 5,
+  name: 'Sanctuary',
+  radius: 10,
+  range: 35,
+  target: 'area',
+  version: '6.400'
+};
