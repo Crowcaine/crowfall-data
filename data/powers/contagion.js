d739e25c73da4d98d6c0236f5d0435c49f68810e
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:08:38 -0500

    feature: add contagion power

---
+++ data/powers/contagion.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 35,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'damage the enemy for 367 damage, if the target is already poisoned this will spread any poisons on them to nearby enemies.',
+  id: 'contagion',
+  name: 'Contagion',
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
