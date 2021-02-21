d93abec40581d05a16859341e7d1331126d61ee3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:21:29 -0500

    feature: add blood feeder disciple powers

---
+++ data/powers/tasty.js
@@ -0,0 +1,10 @@
+export default {
+  castType: 'passive',
+  description:
+    'grants a lifesteal of 10 for 10 seconds when you attack a bleeding target.\nThis cannot trigger more than once every 15 seconds.',
+  duration: 10,
+  id: 'tasty',
+  name: 'Tasty',
+  target: 'self',
+  version: '6.400'
+};
