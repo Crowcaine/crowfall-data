ea43c2c0eb207ee606dc40376c248af993bfa889
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:58:10 -0500

    feature: add nightstalker discipline powers

---
+++ data/powers/feeding-time.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'grants a chance for attacks to send you into a feeding frenzy for 15 seconds, granting [stat:lifesteal:25] for 25 and increasing [stat:attack-power:50] for 50.\nThis effect can only occur once every 20 seconds.',
+  id: 'feeding-time',
+  name: 'Feeding Time',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/vampire-sight.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases your [stat:perception:25] by 25, [stat:far-sight:25] by 25, [stat:stealth:25] and [stat:movement-speed-stealth:25] by 25% during night time.',
+  id: 'vampire-sight',
+  name: 'Vampire Sight',
+  target: 'self',
+  version: '6.400'
+};
