4f7ecf62a1e765b45f22bdbf5e7d01a52651a95a
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 17:55:37 -0500

    feature: add twister of fate discipline powers

---
+++ data/powers/blade-turning.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 76,
+  costType: 'resource',
+  description:
+    'grants a slashing damage immune barrier for 15 seconds.',
+  duration: 15,
+  id: 'blade-turning',
+  name: 'Blade Turning',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/fortification.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 57,
+  costType: 'resource',
+  description:
+    'aids group members and you increasing [stat:health:1000] for 30 seconds.',
+  duration: 30,
+  id: 'fortification',
+  name: 'Fortification',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
