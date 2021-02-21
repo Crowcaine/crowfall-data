fd3b4d2329f08dd42704936045b5cbcfef932130
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 16:54:24 -0500

    feature: add reality tripper discipline powers

---
+++ data/powers/alter-reality.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 50,
+  cost: 2,
+  costType: 'resource',
+  description:
+    'places a heal absorption shield on the target which absorbs incoming heals. In order to remove the shield the target must be healed for 500.\nIf the shield is not healed off within 30 seconds, it will explode dealing 107 nature damage to nearby enemies.',
+  duration: 30,
+  id: 'alter-reality',
+  name: 'Alter Reality',
+  range: 15,
+  target: 'self',
+  version: '6.400'
+};
