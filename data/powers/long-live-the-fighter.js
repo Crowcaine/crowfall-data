07295a5ff68764e5e500ade73d1b401239dce8ad
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 14:09:03 -0500

    feature: add knifegrinder talent powers

---
+++ data/powers/long-live-the-fighter.js
@@ -0,0 +1,11 @@
+export default {
+  castType: 'passive',
+  description:
+    'inflicts a bleed with your third basic attack.\nBleeding causes 169 bleed damage every two seconds for 16 seconds.',
+  id: 'long-live-the-fighter',
+  maxTargets: 1,
+  name: 'Long Live the Fighter',
+  radius: 5,
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/shrapnel.js
@@ -0,0 +1,11 @@
+export default {
+  castType: 'passive',
+  description:
+    'attacking a Bleeding target has a chance to spread the bleed to nearby enemies.\nBleeding causes 169 bleed damage every two seconds for 16 seconds.',
+  id: 'shrapnel',
+  maxTargets: 5,
+  name: 'Shrapnel',
+  range: 5,
+  target: 'self',
+  version: '6.400'
+};
