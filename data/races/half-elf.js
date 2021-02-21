c85de95ffe3ec41f9763dfe0099374df73b6d041
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 15:00:06 -0500

    feature: add half-elf race powers

--- data/races/half-elf.js
+++ data/races/half-elf.js
@@ -9,7 +9,8 @@ export default {
     'stat:dexterity-cap:200 ',
     'stat:intellect-cap:200 ',
     'stat:spirit-cap:200 ',
-    'stat:strength-cap:200'
+    'stat:strength-cap:200',
+    'power:bandits-armor'
   ],
   id: 'half-elf',
   name: 'Half-Elf',
