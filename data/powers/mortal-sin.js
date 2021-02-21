a83bc439c7e251d57aa80541b2f78ee83db7a8f9
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 18:21:50 -0500

    feature: add convictor discipline powers

---
+++ data/powers/mortal-sin.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    "applies Mortal Sin instead of Sin decreasing the enemy's anti-critical strike by 2% per stack.\nAll powers requiring sin benefit from Mortal Sin.",
+  id: 'mortal-sin',
+  name: 'Mortal Sin',
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/prosecute.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 9,
+  cost: 48,
+  costType: 'resource',
+  description:
+    'convicts an enemy causing 247 fire damage. If the target has sin counters, it applies sin equal to the number on your target to enemies within 5m of the target.',
+  id: 'prosecute',
+  name: 'Prosecute',
+  radius: 5,
+  range: 25,
+  target: 'reticle',
+  version: '6.400'
+};
