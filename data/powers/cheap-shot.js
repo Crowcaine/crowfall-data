24b209d6d9b125ec3c6e01b97c669062955d3a90
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 15:14:25 -0500

    feature: add waylayer discipline powers

---
+++ data/powers/cheap-shot.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  cooldown: 14,
+  description:
+    "waylays an enemy from stealth causing 359 damage, stunning them for 5 seconds, and generating pips. This stun is unaffected by the target's resolve or any stun duration reducing skills.",
+  duration: 5,
+  id: 'cheap-shot',
+  name: 'Cheap Shot',
+  range: 5,
+  target: 'reticle',
+  version: '6.400'
+};
---
+++ data/powers/study-target.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'grants a [stat:damage:10] of 10 to abilities used in stealth and the first attack when existing stealth against your target.',
+  id: 'study-target',
+  name: 'Study Target',
+  target: 'self',
+  version: '6.400'
+};
