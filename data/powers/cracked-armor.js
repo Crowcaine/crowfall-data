5596f0338bc5780b2e7aeb5c507f38f359650fa6
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 21:55:07 -0500

    feature: add cracked-armor power

---
+++ data/powers/cracked-armor.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 69,
+  costType: 'resource',
+  description:
+    'enhances group members and you increasing [stat:armor-penetration-crushing:15] by 15% for 30 seconds.',
+  id: 'cracked-armor',
+  maxTargets: 5,
+  name: 'Cracked Armor',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
