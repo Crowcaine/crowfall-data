276c6f7fd627db4e1e3290270594f6598bca21f5
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 21:42:19 -0500

    feature: add nature's guardian discipline powers

---
+++ data/powers/will-o-wisps.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'charged',
+  cooldown: 25,
+  cost: 70,
+  costType: 'essence',
+  description:
+    "charges up and flings a Will o' Wisps to heals allies in an 8m area for 274.",
+  id: 'will-o-wisps',
+  maxTargets: 5,
+  name: "Will O' Wisps",
+  radius: 8,
+  target: 'area',
+  version: '6.400'
+};
