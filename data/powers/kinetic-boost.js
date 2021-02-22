3c522082794b08cf9bffc97ea5c5c4c19b0b83ff
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 21:44:26 -0500

    feature: kinetic bloomer discipline powers

---
+++ data/powers/kinetic-boost.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 68,
+  cost: 39,
+  costType: 'resource',
+  description:
+    'empowers you briefly increasing you [stat:damage:20] by 20 and [stat:power-efficiency:0] by 0 of 20 seconds.',
+  duration: 20,
+  id: 'kinetic-boost',
+  name: 'Kinetic Boost',
+  target: 'self',
+  version: '6.400'
+};
