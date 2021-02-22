2416c6515e780b039a3a5e712442f4bcb0bd90f0
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 20:47:28 -0500

    feature: add minstrel discipline powers

---
+++ data/powers/hymn-of-restoration.js
@@ -0,0 +1,12 @@
+export default {
+  castType: '2 seconds',
+  cooldown: 1,
+  description:
+    "increases your group's [stat:health-regen-in-combat:24] by 24 and [stat:health-regen-out-combat:32] by 32 for 14 seconds. Refreshes every 6 seconds while toggled on.",
+  duration: 14,
+  id: 'hymn-of-restoration',
+  name: 'Hymn of Restoration',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
---
+++ data/powers/verses-of-victory.js
@@ -0,0 +1,12 @@
+export default {
+  castType: '2 seconds',
+  cooldown: 2,
+  description:
+    "increases your group's [stat:attack-power:75] by 75 and [stat:support-power:75] by 75 for 14 seconds. Refreshes every 6 seconds while toggled on.",
+  duration: 14,
+  id: 'verses-of-victory',
+  name: 'Verses of Victory',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
