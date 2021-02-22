9f76adea59d933d572dcae5cd508fcdea503508c
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 22:15:40 -0500

    feature: add shield break power

---
+++ data/powers/shield-breaker.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 9,
+  cost: 9,
+  costType: 'resource',
+  description:
+    'crushes an enemy who is actively using block or parry causing a knockdown and dealing 145 damage.',
+  duration: 30,
+  id: 'shield-breaker',
+  name: 'Shield Breaker',
+  range: 15,
+  target: 'reticle',
+  version: '6.400'
+};
