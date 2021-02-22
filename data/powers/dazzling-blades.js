122cbd4b626bd5ba62f91d4f96f4fc121183d559
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 22:12:42 -0500

    feature: add dazzling-blades power

---
+++ data/powers/dazzling-blades.js
@@ -0,0 +1,16 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 9,
+  costType: 'resource',
+  description:
+    'disorients multiple enemies with an attack reducing their [stat:critical-strike:40] by 40 for 15 seconds and inflicting 246 damage.',
+  duration: 15,
+  id: 'dazzling-blades',
+  maxTargets: 5,
+  name: 'Dazzling Blades',
+  radius: 5,
+  range: 5,
+  target: 'cone',
+  version: '6.400'
+};
