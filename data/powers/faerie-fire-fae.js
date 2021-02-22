f186612224b30cb2eac221afd2cdbaac2538febe
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-22 16:23:30 -0500

    feature: add fae race power

---
+++ data/powers/faerie-fire-fae.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 47,
+  costType: 'resource',
+  description:
+    'lines an enemy in Faerie Fire decreasing their [stat:anti-critical-strike:8] by 8% for 30 seconds. The target cannot stealth while affected.',
+  duration: 30,
+  id: 'faerie-fire-fae',
+  name: 'Faerie Fire',
+  range: 20,
+  target: 'ray',
+  version: '6.400'
+};
--- data/races/fae.js
+++ data/races/fae.js
@@ -9,7 +9,8 @@ export default {
     'stat:dexterity-cap:200 ',
     'stat:intellect-cap:200 ',
     'stat:spirit-cap:200 ',
-    'stat:strength-cap:200'
+    'stat:strength-cap:200',
+    'power:faerie-fire-fae'
   ],
   id: 'fae',
   name: 'Fae',
