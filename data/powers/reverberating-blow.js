d6877449b397f121c86931fb8a5f9c7900939db1
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 20:50:24 -0500

    feature: add reverberating blow power

---
+++ data/powers/reverberating-blow.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 24,
+  cost: 57,
+  costType: 'resource',
+  description:
+    'strikes an enemy for 80 damage and afflicts them with an aura causing other nearby enemies to suffer 187 damage every 2 seconds for 14 seconds.',
+  duration: 14,
+  id: 'reverberating-blow',
+  name: 'Reverberating Blow',
+  range: 5,
+  target: 'cone',
+  version: '6.400'
+};
