65f9d875db1dab773e779eae649bc9f03d6d1dec
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-22 17:14:10 -0500

    feature: add song of speed

---
+++ data/powers/song-of-speed.js
@@ -0,0 +1,13 @@
+export default {
+  castType: '2 secs',
+  cooldown: 3,
+  costType: 'resource',
+  description:
+    "increases the group's [stat:movement-speed:30] and [stat:movement-speed-stealth:30] by 30% for 12 seconds while out of combat and [stat:movement-speed:10] for 10% for 12 seconds while in combat. Refreshes every 6 seconds while toggled on.",
+  duration: 12,
+  id: 'song-of-speed',
+  name: 'Song of Speed',
+  radius: 50,
+  target: 'group',
+  version: '6.400'
+};
