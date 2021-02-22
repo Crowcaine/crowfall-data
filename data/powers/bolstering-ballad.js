2889527d94fdcd997055c291dddbfdeb55170d5c
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 20:14:38 -0500

    feature: add entertainer discipline power

---
+++ data/powers/bolstering-ballad.js
@@ -0,0 +1,11 @@
+export default {
+  castType: 'passive',
+  description:
+    'increases the max hitpoints of all group members by 750 fore 8 seconds each time a song is applied to the group.',
+  duration: 8,
+  id: 'bolstering-ballad',
+  name: 'Bolstering Ballad',
+  radius: 60,
+  target: 'self',
+  version: '6.400'
+};
---
+++ data/powers/reprise.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description: 'increases the area of effect of your songs by 10.',
+  id: 'reprise',
+  name: 'Reprise',
+  target: 'self',
+  version: '6.400'
+};
