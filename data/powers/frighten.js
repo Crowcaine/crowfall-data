f8c4b2fd43b085549d52a7dee92f2334c92bd266
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 22:13:22 -0500

    feature: add intimidator powers

---
+++ data/powers/frighten.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 9,
+  costType: 'resource',
+  description:
+    'shouts at an enemy inflicting 190 damage, reducing their [stat:damage:10] by 10% and [stat:healing:25] by 25.',
+  duration: 15,
+  id: 'frighten',
+  name: 'Frighten',
+  radius: 5,
+  target: 'cone',
+  version: '6.400'
+};
---
+++ data/powers/relentless.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description: 'generates 1.5 rage every 1 second while in combat.',
+  id: 'relentless',
+  name: 'Relentless',
+  target: 'self',
+  version: '6.400'
+};
