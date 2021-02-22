63fcdc01b5432f5d72b8e80feb18253c0014887d
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 20:34:08 -0500

    feature: add unstable mage disciple powers

---
+++ data/powers/wreckoning.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 24,
+  cost: 57,
+  costType: 'resource',
+  description:
+    'causes your barriers to explode, dealing bleed damage to enemies around you equal to the barrier amount you have. Damage is capped to 4420 for each enemy.',
+  id: 'wreckoning',
+  maxTargets: 5,
+  name: 'Wreckoning',
+  radius: 12,
+  target: 'area',
+  version: '6.400'
+};
