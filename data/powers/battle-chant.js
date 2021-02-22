4f8fb7ad7fce2dae71a9bf0168cfa48ff35ec739
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 21:54:33 -0500

    feature: add battle chant power

---
+++ data/powers/battle-chant.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 24,
+  cost: 55,
+  costType: 'resource',
+  description:
+    'summons spinning hammers lasting 15 seconds to damage nearby enemies for 102 damage.',
+  duration: 15,
+  id: 'battle-chant',
+  maxTargets: 5,
+  name: 'Battle Chant',
+  radius: 3,
+  target: 'self',
+  version: '6.400'
+};
