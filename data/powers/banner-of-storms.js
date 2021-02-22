bcce58b8c498a1ffec9f960ca04bbab9e6ecf387
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 20:36:07 -0500

    feature: add blitzer discipline powers

---
+++ data/powers/banner-of-storms.js
@@ -0,0 +1,15 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 76,
+  costType: 'resource',
+  description:
+    'deploys a banner increasing their [stat:resistance-electricity:2500] by 2500, [stat:resistance-ice:2500] by 2500, [stat:damage-electric:10] by 10% and [stat:damage-ice:10] by 10%.',
+  duration: 30,
+  id: 'banner-of-storms',
+  maxTargets: 6,
+  name: 'Banner-of-Storms',
+  radius: 15,
+  target: 'area',
+  version: '6.400'
+};
