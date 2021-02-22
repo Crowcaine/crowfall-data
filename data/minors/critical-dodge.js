bf1b1f959991c3edfcb7839dabf475a4f7659f7f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-22 16:40:15 -0500

    bug: add dark domain to critical dodge minor #4

--- data/minors/critical-dodge.js
+++ data/minors/critical-dodge.js
@@ -3,7 +3,12 @@ export default {
   grants: ['stat:critical-strike:2', 'power:critical-dodge'],
   id: 'critical-dodge',
   name: 'Critical Dodge',
-  requirements: ['domain:shadow', 'domain:wind', 'domain:war'],
+  requirements: [
+    'domain:dark',
+    'domain:shadow',
+    'domain:wind',
+    'domain:war'
+  ],
   type: 'minor',
   version: '6.400'
 };
