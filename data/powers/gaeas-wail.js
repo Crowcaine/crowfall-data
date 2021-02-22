d23bba535c8e113e71cdabd62ad81a7fec88a022
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 21:37:50 -0500

    feature: add spirit speaker discipline powers

---
+++ data/powers/gaeas-wail.js
@@ -0,0 +1,13 @@
+export default {
+  castType: 'instant',
+  cooldown: 9,
+  cost: 53,
+  costType: 'essence',
+  description:
+    'unleaches a bear spirit that may chain to additional enemies up to four time causing 96 nature damage.\nThose hit will also suffr 250 nature damage 2 times over 8 seconds.',
+  id: 'gaeas-wail',
+  name: "Gaea's Wail",
+  range: 30,
+  target: 'reticle',
+  version: '6.400'
+};
