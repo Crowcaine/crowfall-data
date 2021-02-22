3aac2919b7b2efcfef4e4c9df58d514ca85c84ab
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 20:18:45 -0500

    feature: add arkon disciple discipline power

---
+++ data/powers/arkons-fury.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 23,
+  cost: 57,
+  costType: 'resource',
+  description:
+    "unleashes Arkon's Fury on a target dealing 238 damage and applying burning daamge over time to them.\nBurning causes 113 fire damage every two seconds for 16 seconds.",
+  duration: 8,
+  id: 'arkons-fury',
+  name: "Arkon's Fury",
+  range: 25,
+  target: 'rectangle',
+  version: '6.400'
+};
---
+++ data/powers/fiery-brand.js
@@ -0,0 +1,9 @@
+export default {
+  castType: 'passive',
+  description:
+    'gives a chance to apply an Elemental Break to your target when you hit with any fire damage.',
+  id: 'fiery-brand',
+  name: 'Fiery Brand',
+  target: 'self',
+  version: '6.400'
+};
