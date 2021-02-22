49b7fe834102f43861bbfa330460fad0f2c59ebd
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-21 21:33:04 -0500

    feature: add shield figther disciple powers

---
+++ data/powers/brutal-slam.js
@@ -0,0 +1,14 @@
+export default {
+  castType: 'instant',
+  cooldown: 45,
+  cost: 39,
+  costType: 'resource',
+  description:
+    'strikes with your shield for 147 damage and lower their [stat:resistance-all:1000] for 30 seconds.',
+  duration: 30,
+  id: 'brutal-slam',
+  name: 'Brutal Slam',
+  range: 5,
+  target: 'cone',
+  version: '6.400'
+};
---
+++ data/powers/sparring.js
@@ -0,0 +1,8 @@
+export default {
+  castType: 'passive',
+  description: 'increases your [stat:basic-attack-damage:10] by 10%.',
+  id: 'sparring',
+  name: 'Sparring',
+  target: 'self',
+  version: '6.400'
+};
