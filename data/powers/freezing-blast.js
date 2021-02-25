013afc3c05fe7da8198d8c77155176210db5c5c5
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-02-24 20:12:06 -0500

    feature: add freezing blast power

---
+++ data/powers/freezing-blast.js
@@ -0,0 +1,12 @@
+export default {
+  castType: 'instant',
+  cooldown: 9,
+  cost: 12,
+  costType: 'mana',
+  description:
+    'sweeps forward damaging enemies within for 147 damage. Any of you ice within the effect is converted to Frigid Ice.\n\nFrigid Ice harms enemies within 5m over time dazing them and inflicting 103 damage. Enemies destroy it on contact taking 194 damage, becoming rooted and suffering Ice Buildup. Tripping it again while afflicted with Ice Buildup causes knockdown.\n\nYour ice storage capacity determins the number of ice you may have active simultaneously.',
+  id: 'freezing-blast',
+  name: 'Freezing Blast',
+  target: 'rectangle',
+  version: '6.400'
+};
