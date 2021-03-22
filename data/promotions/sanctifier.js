35134f59014d3cef384ce10bb552c6348428da6b
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-03-22 19:57:01 -0400

    bug: fix santifier typo #8

--- data/promotions/sanctifier.js
+++ data/promotions/sanctifier.js
@@ -3,12 +3,12 @@ export default {
   description:
     "Their only desire is to witness the despair in nonbelievers's eyes, so this follwer of Arkon has cast aside the benefits of range to become incredibly defensive.  Confessor powers have their range reduced to 8m and their damage increased by 25%. Fire Tornadoes and Hellfire Blast have their range reduced to 20m.",
   enhances: {
-    'power-hellfire-aura':
-      'has increased damage and hits up 7 targets',
     'power:fervor':
       'grants cc immunity, personal damage modifier and personal healing modifier. Nearby enemies are damage and have sin applied to them. Finally mitigations are no longer reduced but slashing, crushing and piercing resistances are increased',
     'power:fire-tornadoes':
-      'apply a knockdown and no longer consume [hellfire-aura]'
+      'apply a knockdown and no longer consume [hellfire-aura]',
+    'power:hellfire-aura':
+      'has increased damage and hits up 7 targets'
   },
   grants: [
     'proficiency:mail',
