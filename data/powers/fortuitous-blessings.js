44d23f9f47cbdc14a7183c16a98699e1c3465cba
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-03-22 20:04:02 -0400

    bug: type in friar #10

--- data/powers/fortuitous-blessings.js
+++ data/powers/fortuitous-blessings.js
@@ -1,7 +1,7 @@
 export default {
   castType: 'passive',
   description:
-    "periodically grants the Seekers' Blessing for movement speed if you have not recently healer, Impoverished Blessing for restoring your resource over time if you have healed and are low resource and Charitable Blessing for support power otherwise.",
+    "periodically grants the Seekers' Blessing for movement speed if you have not recently healed, Impoverished Blessing for restoring your resource over time if you have healed and are low resource and Charitable Blessing for support power otherwise.",
   id: 'fortuitous-blessings',
   name: 'Fortuitous Blessings',
   target: 'self',
