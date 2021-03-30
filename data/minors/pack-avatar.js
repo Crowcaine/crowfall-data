23404e8a83f1dd1993382b62cf43b4507671b506
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-03-30 15:42:24 -0400

    feature: add pack-avatar to half-elfs #12

--- data/minors/pack-avatar.js
+++ data/minors/pack-avatar.js
@@ -3,7 +3,7 @@ export default {
   grants: ['stat:health:500', 'power:pack-avatar'],
   id: 'pack-avatar',
   name: 'Pack Avatar',
-  requirements: ['race:centaur', 'race:elken'],
+  requirements: ['race:centaur', 'race:elken', 'race:half-elf'],
   type: 'minor',
   version: '6.400'
 };
