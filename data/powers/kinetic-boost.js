db8d0ffbc584130ec4958387895b9dbb4ef1446e
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 19:17:31 -0400

    update: kinetic bloomer 6.530

--- data/powers/kinetic-boost.js
+++ data/powers/kinetic-boost.js
@@ -1,13 +1,13 @@
 export default {
   castType: 'instant',
-  cooldown: 68,
+  cooldown: 60,
   cost: 39,
   costType: 'resource',
   description:
-    'empowers you briefly increasing you [stat:damage:20] by 20 and [stat:power-efficiency:0] by 0 of 20 seconds.',
+    'empowers you briefly increasing you [stat:damage:20] by 20, [stat:power-efficiency:0] by 20%, [stat:healing:20] by 10%  and [stat:power-damage-ranged:10] by 10% for 20 seconds.',
   duration: 20,
   id: 'kinetic-boost',
   name: 'Kinetic Boost',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
