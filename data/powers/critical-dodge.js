d7888e6b27ff25b974f989e9de7e9c41d075dedf
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 23:05:01 -0400

    update: critical dodge 6.530

--- data/powers/critical-dodge.js
+++ data/powers/critical-dodge.js
@@ -1,9 +1,9 @@
 export default {
   castType: 'passive',
   description:
-    'recovers one dodge when hitting critically.\nThis effect can occur once per 30 seconds.',
+    'reset one dodge when hitting critically.\n\nThis effect can occur once per 30 seconds.',
   id: 'critical-dodge',
   name: 'Critical Dodge',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
