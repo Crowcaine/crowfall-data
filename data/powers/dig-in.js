4a482bbe1bce183457a36aeecf4373faf26d7e9f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 22:59:57 -0400

    update: defender 6.530

--- data/powers/dig-in.js
+++ data/powers/dig-in.js
@@ -1,9 +1,9 @@
 export default {
   castType: 'passive',
   description:
-    'buffs [stat:anti-critical-strike:100] by 100% for 15 seconds when you are hit under 25% health.\nThis effect can only occur once every 10 seconds.',
+    'buffs [stat:anti-critical-strike:100] by 100% for 15 seconds when you are hit under 25% health.\n\nThis effect can only occur once every 10 seconds.',
   id: 'dig-in',
   name: 'Dig In',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
