32cd6eb772f52210934ceba3b5702092f26bf1c4
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-31 00:00:54 -0400

    update: exorcist 6.530

--- data/powers/blessed-symbol.js
+++ data/powers/blessed-symbol.js
@@ -2,8 +2,9 @@ export default {
   castType: 'passive',
   description:
     'increases your [stat:critical-healing-amount:15] by 15% and [stat:critical-healing-amount:15] by 15% for 20 seconds when you drop below 50% health. This cannot occur more than once every 30 seconds.',
+  duration: 20,
   id: 'blessed-symbol',
   name: 'Blessed Symbol',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
