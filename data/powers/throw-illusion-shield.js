b65218372b60a3fc6b76635f19456707ae99d34d
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 22:05:03 -0400

    update: illusory shield 6.530

--- data/powers/throw-illusion-shield.js
+++ data/powers/throw-illusion-shield.js
@@ -4,11 +4,11 @@ export default {
   cost: 92,
   costType: 'resource',
   description:
-    'throws an illusion shield at your target for 161 damage, snares the target and causes it to richochet off to two additional targets.\nDamage is reduced by 30% per each ricochet up to a maximum of 60% reduction.',
+    'throws an illusion shield at your target for 161 damage, roots the target and causes it to richochet off to two additional targets.\n\nDamage is reduced by 30% per each ricochet up to a maximum of 60% reduction.',
   id: 'throw-illusion-shield',
   maxTargets: 3,
   name: 'Throw Illusion Shield',
   range: 15,
   target: 'cone',
-  version: '6.400'
+  version: '6.530'
 };
