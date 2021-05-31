ab9890002d0384ac84be5d123e00448a969952fa
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 23:43:30 -0400

    update: mudman 6.530

--- data/powers/jelly-skin.js
+++ data/powers/jelly-skin.js
@@ -4,10 +4,10 @@ export default {
   cost: 144,
   costType: 'resource',
   description:
-    'protects you for 15 seconds with skin like rubber granting you a barrier with immunity to Crushing damage.',
+    'protects you for 15 seconds with skin like rubber granting you a barrier with immunity to crushing damage and healing you for 267 health 3 times over 8 seconds after the barrier is gone.',
   duration: 15,
   id: 'jelly-skin',
   name: 'Jelly Skin',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
--- data/powers/muck-spatter.js
+++ data/powers/muck-spatter.js
@@ -1,15 +1,15 @@
 export default {
   castType: 'instant',
-  cooldown: 30,
+  cooldown: 26,
   cost: 144,
   costType: 'resource',
   description:
-    'empowers you for 15 seconds and causes Crushing attacks made against you to afflict nearby enemies with Blindness and Corruption. This can only occur once every three seconds.\nCorruption causes 250 Nature damage 2 times over 8 seconds.',
-  duration: 15,
+    'empowers you for 18 seconds and causes Crushing attacks made against you to afflict nearby enemies with Blindness and Corruption. This can only occur once every three seconds.\nCorruption causes 250 Nature damage 2 times over 8 seconds.',
+  duration: 18,
   id: 'muck-spatter',
   maxTargets: 5,
   name: 'Muck Spatter',
   radius: 8,
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
