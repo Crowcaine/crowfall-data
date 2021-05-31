3c391c992e0aa771aa77c99a720f80d2e309f25f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-31 00:09:42 -0400

    update: sun worshipper 6.530

--- data/majors/sun-worshiper.js
+++ data/majors/sun-worshiper.js
@@ -3,12 +3,12 @@ export default {
   grants: [
     'stat:resistance-elemental:300',
     'stat:support-power:100',
-    'power:soothing-winds',
+    'power:chain-heal',
     'power:cleanse'
   ],
   id: 'sun-worshiper',
   name: 'Sun Worshiper',
   requirements: ['race:centaur', 'domain:light', 'domain:fire'],
   type: 'major',
-  version: '6.400'
+  version: '6.530'
 };
