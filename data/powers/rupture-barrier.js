4507ac964fd4d102e9d789c26ed8bea6055fc5a3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 22:37:13 -0400

    update: rupture barrier 6.530

--- data/powers/rupture-barrier.js
+++ data/powers/rupture-barrier.js
@@ -1,9 +1,9 @@
 export default {
   castType: 'passive',
   description:
-    'causes an additional 50% weapon damage when striking a barrier and restore your resource.',
+    'deals an additional 50% weapon damage when striking a barrier and restore your resource.',
   id: 'rupture-barrier',
   name: 'Rupture Barrier',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
