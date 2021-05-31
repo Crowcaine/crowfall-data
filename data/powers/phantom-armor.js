693beed28d4ae1ff4a164082b05109da648c24a9
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 22:34:54 -0400

    update: phantom armor 6.530

--- data/powers/phantom-armor.js
+++ data/powers/phantom-armor.js
@@ -1,10 +1,10 @@
 export default {
   castType: 'passive',
   description:
-    'has a chance to increase your armor class by 3500 for 12 seconds when an enemy damages you.\nThis effect can only occur once every 60 seconds.',
+    'has a chance to increase your armor class by 3500 for 12 seconds when an enemy damages you.\n\nThis effect can only occur once every 60 seconds.',
   duration: 12,
   id: 'phantom-armor',
   name: 'Phantom Armor',
   target: 'self',
-  version: '6.400'
+  version: '6.530'
 };
