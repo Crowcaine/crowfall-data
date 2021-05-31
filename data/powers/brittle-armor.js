90805657d3120461316e29abc476ece5b3111419
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 20:09:47 -0400

    update: tundra mage 6.530

--- data/powers/brittle-armor.js
+++ data/powers/brittle-armor.js
@@ -4,11 +4,11 @@ export default {
   cost: 84,
   costType: 'resource',
   description:
-    'causes 226 damage and debuffs their final physical mitigation by 20 for 30 seconds.',
+    'causes 226 damage and debuffs their [stat:resistance-all:1000] by 1000 for 30 seconds.',
   duration: 30,
   id: 'brittle-armor',
   name: 'Brittle Armor',
   range: 30,
   target: 'reticle',
-  version: '6.400'
+  version: '6.530'
 };
--- data/powers/freezing-storm.js
+++ data/powers/freezing-storm.js
@@ -5,10 +5,11 @@ export default {
   costType: 'resource',
   description:
     'unleashes a freezing storm under the ground targeted area repeatedly inflicting 119 damage to enemies and slowing them. Applies a stack of freezing every second, reaching 5 stacks of freezing will stun the target.',
+  duration: 5,
   id: 'freezing-storm',
   maxTargets: 5,
   name: 'Freezing Storm',
   range: 30,
   target: 'area',
-  version: '6.400'
+  version: '6.530'
 };
