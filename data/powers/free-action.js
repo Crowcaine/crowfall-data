0ebdebb9fee5b0598965f2245132b86384fac7a8
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 23:39:06 -0400

    update: freedom fighter 6.530

--- data/powers/free-action.js
+++ data/powers/free-action.js
@@ -4,11 +4,11 @@ export default {
   cost: 72,
   costType: 'resource',
   description:
-    'liberates group members and yourself gaining immunity to crowd control effects for 9 seconds.\nThis attack can be instantly flash-cast.',
+    'liberates group members and yourself gaining immunity to crowd control effects for 9 seconds.\n\nThis attack can be instantly flash-cast.\n\nThis effect can only be applied to each target once every 30 seconds.',
   duration: 9,
   id: 'free-action',
   name: 'Free Action',
   radius: 50,
   target: 'group',
-  version: '6.400'
+  version: '6.530'
 };
