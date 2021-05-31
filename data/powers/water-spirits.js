786545fdd737baaad68ccd6b44cc3008289c734f
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-05-30 21:45:25 -0400

    update: hydrologist 6.530

--- data/powers/water-spirits.js
+++ data/powers/water-spirits.js
@@ -4,12 +4,12 @@ export default {
   cost: 174,
   costType: 'resource',
   description:
-    'summons and launch a water spirit forward which heals allies within range of the water spirit.\nWater spirits last 20 seconds or 30 heals.\nWater spirits only heals each player a maximum of 5 times.\n Enemies hit by the water spirit suffer a 30% to [stat:damage-fire:30%] for 20 seconds.',
+    'summons and launch a water spirit forward which heals allies within range of the water spirit for 67 healing per heal when fully charged.\n\nWater spirits last 20 seconds or 30 heals.\n\nWater spirits only heals each player a maximum of 5 times.\n Enemies hit by the water spirit suffer debuff of [stat:damage-fire:30%] by 30% for 20 seconds.',
   duration: 20,
   id: 'water-spirits',
   maxTargets: 5,
-  movement: 4,
   name: 'Water Spirits',
+  radius: 7,
   target: 'area',
-  version: '6.400'
+  version: '6.530'
 };
