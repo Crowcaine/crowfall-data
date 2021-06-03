08d87259c463ced590d385258d9d666c2450dc01
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-02 21:35:43 -0400

    update: shock trooper 6.530

--- data/majors/shock-trooper.js
+++ data/majors/shock-trooper.js
@@ -10,5 +10,5 @@ export default {
   name: 'Shock Trooper',
   requirements: ['domain:justice'],
   type: 'major',
-  version: '6.400'
+  version: '6.530'
 };
--- data/powers/banner-of-storms.js
+++ data/powers/banner-of-storms.js
@@ -4,7 +4,7 @@ export default {
   cost: 39,
   costType: 'resource',
   description:
-    'deploys a banner increasing their [stat:resistance-electricity:2500] by 2500, [stat:resistance-ice:2500] by 2500, [stat:damage-electricity:10] by 10% and [stat:damage-ice:10] by 10% of up to 8 allies in the radius.',
+    'deploys a banner increasing [stat:resistance-electricity:2500] by 2500, [stat:resistance-ice:2500] by 2500, [stat:damage-electricity:10] by 10% and [stat:damage-ice:10] by 10% of up to 8 allies in the radius.',
   duration: 30,
   id: 'banner-of-storms',
   maxTargets: 8,
