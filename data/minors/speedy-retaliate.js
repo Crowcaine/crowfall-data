3d54bcba31391783744f19c54b79b41af33fc148
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-02 22:54:12 -0400

    update: speedy retaliate 6.530

--- data/minors/speedy-retaliate.js
+++ data/minors/speedy-retaliate.js
@@ -5,5 +5,5 @@ export default {
   name: 'Speedy Retaliate',
   requirements: ['class:champion', 'domain:wind'],
   type: 'minor',
-  version: '6.400'
+  version: '6.530'
 };
--- data/powers/speedy-retaliate.js
+++ data/powers/speedy-retaliate.js
@@ -4,12 +4,12 @@ export default {
   cost: 65,
   costType: 'stamina',
   description:
-    'removes crowd control effects and returns you to your feet with an attack inflicting 194 damage and then increasing your [stat:movement-speed:30] by 30% for 8 seconds.',
+    'attacks inflicting 290 damage. If used while crowd controlled this increases your [stat:movement-speed:30] by 30% for 8 seconds. This power can be flashcast, removes crowd control effect and pauses your stamina regen for 3 seconds.',
   duration: 8,
   id: 'speedy-retaliate',
   maxTargets: 5,
   name: 'Speedy Retaliate',
   radius: 5,
   target: 'area',
-  version: '6.400'
+  version: '6.530'
 };
