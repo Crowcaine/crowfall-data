f7a6703e29e84f54cdf31fc44bb6f7c7d43e2b08
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-02 22:04:03 -0400

    update: dirge of dissonance 6.530

--- data/powers/dirge-of-dissonance.js
+++ data/powers/dirge-of-dissonance.js
@@ -2,12 +2,12 @@ export default {
   castType: '2 secs',
   cooldown: 1,
   description:
-    'sings for 14 seconds damaging up to 5 enemies within 5 meters for 48 damage every 3 seconds.',
-  duration: 14,
+    'sings for 12 seconds damaging up to 5 enemies within 5 meters for 48 damage every 3 seconds.',
+  duration: 12,
   id: 'dirge-of-dissonance',
   maxTargets: 5,
   name: 'Dirge of Dissonance',
   radius: 5,
   target: 'pbae',
-  version: '6.400'
+  version: '6.530'
 };
