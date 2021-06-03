72ce54a895bd8069b2afc9989b2fea5e7e715635
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-02 21:53:22 -0400

    update: song of speed 6.530

--- data/minors/song-of-speed.js
+++ data/minors/song-of-speed.js
@@ -5,5 +5,5 @@ export default {
   name: 'Song of Speed',
   requirements: ['domain:music'],
   type: 'minor',
-  version: '6.400'
+  version: '6.530'
 };
--- data/powers/song-of-speed.js
+++ data/powers/song-of-speed.js
@@ -1,6 +1,6 @@
 export default {
   castType: '2 secs',
-  cooldown: 3,
+  cooldown: 1,
   costType: 'resource',
   description:
     "increases the group's [stat:movement-speed:30] and [stat:movement-speed-stealth:30] by 30% for 12 seconds while out of combat and [stat:movement-speed:10] for 10% for 12 seconds while in combat. Refreshes every 6 seconds while toggled on.",
@@ -9,5 +9,5 @@ export default {
   name: 'Song of Speed',
   radius: 50,
   target: 'group',
-  version: '6.400'
+  version: '6.530'
 };
