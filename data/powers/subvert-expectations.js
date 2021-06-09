852dfad5ad444f0d06b384c597023f04560171c3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-08 20:38:54 -0400

    bug: fix subvert expectation attack-power value

--- data/powers/subvert-expectations.js
+++ data/powers/subvert-expectations.js
@@ -1,7 +1,7 @@
 export default {
   castType: 'passive',
   description:
-    'applies [stat:attack-power] with the buff amount increasing as your resource pool decreases.',
+    'applies [stat:attack-power:0] with the buff amount increasing as your resource pool decreases.',
   id: 'subvert-expectations',
   name: 'Subvert Expectations',
   target: 'self',
