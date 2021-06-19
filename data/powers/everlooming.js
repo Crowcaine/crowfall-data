b3103db2bfd8ee9f9bb3456942b4693a95784602
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-19 14:21:14 -0400

    update: unholy beast 6.540

--- data/powers/everlooming.js
+++ data/powers/everlooming.js
@@ -4,5 +4,5 @@ export default {
   id: 'everlooming',
   name: 'Everlooming',
   target: 'self',
-  version: '6.400'
+  version: '6.540'
 };
--- data/powers/undead-fortitude.js
+++ data/powers/undead-fortitude.js
@@ -4,5 +4,5 @@ export default {
   id: 'undead-fortitude',
   name: 'Undead Fortitude',
   target: 'self',
-  version: '6.400'
+  version: '6.540'
 };
