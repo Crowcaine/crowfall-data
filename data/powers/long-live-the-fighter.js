49f4c1dff0a10db0f63123ecc180bd4b74d954a9
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-19 15:46:31 -0400

    update: knife grinder 6.540

--- data/powers/long-live-the-fighter.js
+++ data/powers/long-live-the-fighter.js
@@ -1,11 +1,11 @@
 export default {
   castType: 'passive',
   description:
-    'inflicts a bleed with your third basic attack.\nBleeding causes 169 bleed damage every two seconds for 16 seconds.',
+    'inflicts a bleed with your third basic attack.\nBleeding causes 74 bleed damage every two seconds for 16 seconds.',
   id: 'long-live-the-fighter',
   maxTargets: 1,
   name: 'Long Live the Fighter',
   radius: 5,
   target: 'self',
-  version: '6.400'
+  version: '6.540'
 };
--- data/powers/shrapnel.js
+++ data/powers/shrapnel.js
@@ -1,11 +1,11 @@
 export default {
   castType: 'passive',
   description:
-    'attacking a Bleeding target has a chance to spread the bleed to nearby enemies.\nBleeding causes 169 bleed damage every two seconds for 16 seconds.',
+    'attacking a Bleeding target has a chance to spread the bleed to nearby enemies.\nBleeding causes 74 bleed damage every two seconds for 16 seconds.',
   id: 'shrapnel',
   maxTargets: 5,
   name: 'Shrapnel',
   range: 5,
   target: 'self',
-  version: '6.400'
+  version: '6.540'
 };
