6589bb5e0a87ee8b6bb3b9f4f697a2e1340ba4de
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-19 12:45:32 -0400

    update: salt-peter 6.540

--- data/powers/saltpeter-rounds.js
+++ data/powers/saltpeter-rounds.js
@@ -1,11 +1,11 @@
 export default {
   castType: 'instant',
-  cooldown: 26,
+  cooldown: 24,
   description:
     'empowers pistol attacks to inflict slow on your target and increases your [stat:ranged-distance:5] by 5 for 20 seconds.',
   duration: 20,
   id: 'saltpeter-rounds',
   name: 'Saltpeter Rounds',
   target: 'self',
-  version: '6.400'
+  version: '6.540'
 };
