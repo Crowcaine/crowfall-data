ff0f4dc5f287678f757a2a6dd6299e2602c202d3
Filipe Araujo <filipe.alves.araujo@gmail.com>
2021-06-19 13:37:12 -0400

    update: gapfinder 6.540

--- data/powers/between-the-seams.js
+++ data/powers/between-the-seams.js
@@ -11,5 +11,5 @@ export default {
   name: 'Between the Seams',
   radius: 50,
   target: 'group',
-  version: '6.530'
+  version: '6.540'
 };
--- data/powers/pin-cushion.js
+++ data/powers/pin-cushion.js
@@ -9,5 +9,5 @@ export default {
   id: 'pin-cushion',
   name: 'Pin Cushion',
   target: 'self',
-  version: '6.530'
+  version: '6.540'
 };
